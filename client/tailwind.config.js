88ebf71adf884497b45b1122f79e756f9d77c0ab
Daniel <danielseniv@outlook.com>
2023-07-18 21:35:13 -0400

    Initialize tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ChakraPetch: ["Chakra Petch","sans-serif"]
+      },
+      backgroundImage: {
+        'hero': "url('/landing_page.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
