00a2f842e113f7380b870c8fa42df8f482ca8449
Daniel <danielseniv@outlook.com>
2023-07-20 23:06:40 -0400

    Import Jordan 3D stage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,11 @@
-function App() {
+import Jordan from "./canvas/Jordan"
+import "./app.css"
 
+function App() {
   return (
     <section className='home'>
-      <div className="home-img-container">
-        <h1 className="home-page-header-left"> 
-          The <br/> Drop
-        </h1>
-        <h1 className="home-page-header-right">
-          Is <br/>
-          Live
-        </h1>
-      </div>
-    </section> 
+        <Jordan />
+    </section>
   )
 }
 
