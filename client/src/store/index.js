838346ed752e48f76c70fd0eb7be169d09cfa2c8
Daniel <danielseniv@outlook.com>
2023-07-26 18:18:40 -0400

    Add state for shoe string to center object when shoe state changes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,8 @@ import { proxy } from 'valtio';
 
 const state = proxy({
     intro: true,
-    shoes: [], //clicking a show will set in on the stage, and present it. Intro is false so then go to shoe selected
+    shoe: "",
+
 })
 
 export default state;
