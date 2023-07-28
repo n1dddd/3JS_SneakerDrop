05076485733499f14360421fdc89c75ab7155f1c
Daniel <danielseniv@outlook.com>
2023-07-27 22:11:02 -0400

    Add animation functions, add constants used for descriptions of shoes

---
+++ client/src/config/constants.js
@@ -0,0 +1,67 @@
+export const shoeDescriptions = {
+    jordan1Sbb:{ 
+        description: `
+            The Jordan 1 Shattered Backboard Colorway pays homage to a historic moment in Michael Jordan's basketball career. 
+            In August 1985, during an exhibition game in Trieste, Italy, Jordan's athleticism and power were on full display 
+            when he executed a jaw-dropping dunk that shattered the glass of the backboard. The colors he wore during that 
+            iconic moment were black, white, and orange. Inspired by this significant event, the Jordan brand released the Jordan 
+            1 High Shattered Backboard in 2015. The colorway of the shoe perfectly captures the black, white, and orange hues 
+            that adorned Jordan's feet during the game in Italy. The combination of these colors symbolizes the intensity, 
+            impact, and energy of that memorable dunk, making it a sought-after and cherished sneaker among fans and sneakerheads alike.
+            Due to the overwhelming popularity of the original release, in 2016, the Jordan 1 High Reverse Shattered Backboard, also known as 
+            the Jordan 1 High Shattered Backboard 2.0, hit the market. This alternate version features a color scheme that is flipped or 
+            reversed compared to the original, but still captures the essence and spirit of the legendary shattered backboard moment.
+    `
+    },
+    jordan1Obs: {
+        description: `
+            The Jordan 1 Obsidian Colorway pays tribute to the classic Air Jordan 1 silhouette with a fresh and captivating 
+            twist. Nike reintroduces this iconic shoe in a striking "Obsidian" colorway, designed as a nod to Michael Jordan's alma 
+            mater, the University of North Carolina. The captivating blue tones on these sneakers draw inspiration from the colors of 
+            the university where Jordan made his mark as a young basketball star. The "Obsidian" Jordan 1 not only commemorates Jordan's 
+            college days but also encapsulates a timeless style that seamlessly blends sport and fashion. Whether you're hitting the 
+            basketball courts or strolling the streets, these sneakers exude a sense of sporting heritage combined with contemporary flair.
+            The Jordan 1 Obsidian colorway is a true collector's item that celebrates Michael Jordan's journey to 
+            greatness and adds a touch of his alma mater's spirit to your footwear collection. With its rich history and timeless design, 
+            this shoe continues to captivate sneaker enthusiasts and sports fans alike, making it a must-have for anyone looking to embrace 
+            both style and legacy.
+        `
+    },
+    jordan1Chg: {
+        description:` 
+            The Air Jordan 1 Chicago is an iconic sneaker that holds a special place in the history of basketball and sneaker culture. 
+            Crafted by designer Peter Moore, the Air Jordan 1 marked the beginning of Michael Jordan's historic collaboration with Nike.
+            In the 1980s, basketball shoes typically featured subdued colors, reserved primarily for women's aerobics or running footwear.
+            Peter Moore was determined to challenge this norm and revolutionize the basketball sneaker landscape. During Michael Jordan's 
+            rookie year with the Chicago Bulls, he was initially wearing Nike Air Ships, which bore a striking resemblance to what would become
+            the Air Jordan 1. The black and red colorway of the shoes he wore on the court caught the NBA's attention and resulted in an 
+            unprecedented $5000 fine for breaking the league's uniform guidelines. Despite the controversy, the Air Jordan 1 officially debuted in 
+            1985, featuring the iconic "Chicago" colorway. This bold and distinctive combination of red, white, and black became an instant hit, 
+            capturing the attention of fans and sneaker enthusiasts worldwide. The Air Jordan 1 Chicago's release marked the beginning of a legendary
+            legacy that endures to this day
+        `
+    },
+    jordan1Uni: 
+        {description:`
+            The Jordan 1 Uni, also known as the "UNC" (University of North Carolina) colorway, holds a special place in sneaker history due to its
+            deep connection with Michael Jordan's alma mater, the University of North Carolina at Chapel Hill. Jordan's admiration for the university
+            and the opportunities it provided him are well-documented, as he has often expressed gratitude for being recruited by UNC and fulfilling
+            his dreams of playing Division I basketball. Throughout his illustrious career and even after retirement, Michael Jordan has maintained 
+            a strong bond with the University of North Carolina. He has continued to give back to the institution that played a pivotal role in his 
+            life and career. Jordan's loyalty and appreciation for his blue UNC jersey have been evident in his involvement with the university, which 
+            includes regular visits to the campus, offering advice and guidance to players, and supporting the management of UNC's NCAA basketball team.
+            As time has passed, the "UNC" colorway has solidified itself as a classic in sneaker history. Its association with Michael Jordan's collegiate 
+            roots and the sentimental value it holds for him and his fans have made it a cherished and sought-after style.
+    `
+    },
+    jordan1BnW: 
+        {description: `
+            The Air Jordan 1 "Black White" is a timeless high-top sneaker that holds a significant place in the history of footwear and basketball culture.
+            As the first signature shoe of the legendary Michael Jordan, it made its initial debut in 1985 alongside the iconic Chicago colorway, forever
+            altering the landscape of athletic footwear.Over the years, the Air Jordan 1 "Black White" has remained a highly sought-after and coveted sneaker 
+            among sneaker enthusiasts, basketball fans, and fashion aficionados alike. Its enduring popularity has led to numerous re-releases and special 
+            editions, allowing new generations to experience the rich history and legacy that this iconic shoe carries. The Air Jordan 1 "Black White" stands 
+            as a symbol of innovation, style, and Michael Jordan's groundbreaking influence on both the game of basketball and the world of sneakers. Its 
+            timeless design and historical significance continue to inspire and captivate sneakerheads and athletes worldwide.
+        `}
+}
