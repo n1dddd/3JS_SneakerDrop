af70a1a95b8d3a7bc670fb856aa2abf1e9e4516f
Daniel <danielseniv@outlook.com>
2023-07-27 22:07:30 -0400

    Add Home and Descriptions components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import Index from "./canvas/index"
+import Home from "./pages/Home"
+import Descriptions from "./pages/Descriptions"
 import "./app.css"
+
 
 function App() {
   return (
     <section className='home'>
+        <Home />
         <Index />
+        <Descriptions />
     </section>
   )
 }
