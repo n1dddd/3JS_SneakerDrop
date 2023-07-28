68f3c59778afa45702e5cb2e8e2f0cdb751b7dc8
Daniel <danielseniv@outlook.com>
2023-07-27 22:10:15 -0400

    Add breakpoints, change font style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,9 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xsm': '100px',
+      // => @media (min-width: 640px) { ... }
+      
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
-        ChakraPetch: ["Chakra Petch","sans-serif"]
+        Karla: ["Karla","sans-serif"]
       },
       backgroundColor: {
          'bg-color': 'rgb(211,235,255)',
