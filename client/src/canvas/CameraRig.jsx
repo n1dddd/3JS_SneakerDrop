a2060595344c138357d9d19ec48a19d435d3def2
Daniel <danielseniv@outlook.com>
2023-08-08 15:07:24 -0400

    Add varying breakpoints for where camera should be based on viewport size

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -10,14 +10,28 @@ const CameraRig = ({ children}) => {
     const group = useRef();
     const snap = useSnapshot(state);
 
+    const isBreakpoint1280 = window.innerWidth <= 1280;
+    const isBreakpoint960 = window.innerWidth <= 960;
+    const isBreakpoint760 = window.innerWidth <= 760;
+    const isBreakpointMobile = window.innerWidth <= 540;
+
+    let allShoesTargetPos = [0, 0, 0.6];
+    let singleShoeTargetPos = [-0.18, 0, 0.35];
+
     useFrame((state, delta) => {
         if(snap.intro) {
-            easing.damp3(state.camera.position,  [0, 0, 0.6], 0.25, delta)
+            if(isBreakpoint960) allShoesTargetPos = [0, 0, 0.8]
+            if(isBreakpoint760) allShoesTargetPos = [0, 0, 1]
+            if(isBreakpointMobile) allShoesTargetPos = [0, 0, 1.4]
+            easing.damp3(state.camera.position, allShoesTargetPos, 0.3, delta)
+
         }
         else {
-            easing.damp3(state.camera.position,  [-0.22, 0, 0.35], 0.25, delta)
+            if(isBreakpoint1280) singleShoeTargetPos = [0, -0.15, 0.5];
+            easing.damp3(state.camera.position, singleShoeTargetPos , 0.3, delta)
         }
     })
+    
     return (
         <group ref={group}>{children}</group>
     )
