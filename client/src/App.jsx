fa7f39dd82af2cd6d47071757ee678419e38ab40
Daniel <danielseniv@outlook.com>
2023-08-16 22:40:22 -0400

    Add suspense wrapper to load components together, add LoadingScreen component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import Index from "./canvas/index"
 import Home from "./pages/Home"
 import Descriptions from "./pages/Descriptions"
+import LoadingScreen from "./components/LoadingScreen"
 import "./app.css"
+import { Suspense, useState } from "react"
 
 
 function App() {
+
+  const [start, setStart] = useState(false);
   return (
     <section className='home'>
+      <Suspense fallback={null}>
         <Home />
         <Index />
         <Descriptions />
+      </Suspense>
+      <LoadingScreen started={start} onStarted={() => setStart(true)} />
     </section>
   )
 }
